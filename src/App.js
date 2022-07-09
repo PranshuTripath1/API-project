3dc54ed70f951ef2a0e24799166886d5cda1e0d1
Pranshu Tripathi <108667371+PranshuTripath1@users.noreply.github.com>
2022-07-09 15:28:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { UserContainer } from "./UserContainer";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>API Project</h1>
+        <UserContainer />
+      </div>
+      <h2 style={{ textAlign: "center" }}>Created by Pranshu Tripathi.</h2>
+    </>
+  );
+}
